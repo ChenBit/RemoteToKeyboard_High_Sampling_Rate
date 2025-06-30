98c4d980d92199ac465906b8db59e2daa89c1f0b
Eric Chen <108616001+ChenBit@users.noreply.github.com>
2025-06-30 18:44:12 +0800

    Update WASD_With_Shift.cpp

--- WASD_With_Shift.cpp
+++ WASD_With_Shift.cpp
@@ -120,7 +120,7 @@ int main() {
         }
 
         // Sleep for ~10ms to achieve 100Hz polling rate
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
     return 0;
